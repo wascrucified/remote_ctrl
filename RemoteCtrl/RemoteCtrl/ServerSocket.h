b95b3b034ff22184334b3667b425ea7882f3a4af
wascrucified <19802482587@163.com>
2025-11-23 21:32:24 +0800

    完成鼠标消息的处理

--- RemoteCtrl/RemoteCtrl/ServerSocket.h
+++ RemoteCtrl/RemoteCtrl/ServerSocket.h
@@ -133,6 +133,19 @@ public:
     std::string strOut; // 整个包的数据
 };
 #pragma pack(pop)
+   
+typedef struct MouseEvent {
+    MouseEvent() {
+        nAction = 0;
+        nButton = -1;
+        ptXY.x = 0;
+        ptXY.y = 0;
+    }
+
+    WORD nAction;  // 点击、移动、双击
+    WORD nButton;  // 左键、右键、中键
+    POINT ptXY;    // 坐标
+} MOUSEEV, * PMOUSEEV;
 
 // 服务器套接字类
 class CServerSocket
@@ -221,6 +234,14 @@ public:
     bool GetFilePath(std::string& strPath) {
         if ((m_packet.sCmd >= 2) && (m_packet.sCmd <= 4)) {//2 代表选择获取文件列表功能的标识
             strPath = m_packet.strData;
+            return true;
+        }
+        return false;
+    }
+    
+    bool GetMouseEvent(MOUSEEV& mouse) {
+        if (m_packet.sCmd == 5) {
+            memcpy(&mouse, m_packet.strData.c_str(), sizeof(MOUSEEV));
             return true;
         }
         return false;
